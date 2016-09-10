4a90c5e4fe5969a9b2558d3c8f23a02b837d9747
Plazmius <artemxl@gmail.com>
2016-09-10 22:56:50 +0300

    First Commit

---
+++ SeaFight/SeaFight/Field.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeaFight
+{
+    class Field : Control
+    {
+        private const int cellSize = 25;
+
+        private FieldCell[,] field = new FieldCell[10, 10];
+
+        Field()
+        {
+
+        }
+    }
+}
---
+++ SeaFight/SeaFight/FieldCell.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeaFight
+{
+    class FieldCell : Label
+    {
+        private int X;
+        private int Y;
+
+        FieldCell(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
